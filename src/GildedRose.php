8bb1b64d37adf5ed7653f05d29029abbb2d19a3a
Miguel <miguel.s.address@gmail.com>
2015-05-28 22:16:25 +0200

    Now the updateQuality method decreases SellIn and updates the quality qith the delta calculated in the new method

--- src/GildedRose.php
+++ src/GildedRose.php
@@ -17,17 +17,8 @@ class GildedRose {
         $delta = 0;
         switch ($item->name) {
             case 'Aged Brie':
-                // The $quality of an item is never negative
-                if ($item->quality < 0) {
-                    $delta = 0;
-                }
-                //The $quality of an item is never more than 50
-                else if ($item->quality < 50) {
-                    $delta = 1;
-                } else {
-                    // "Aged Brie" actually increases in $quality the older it gets
-                    $delta = 1;
-                }
+                // "Aged Brie" actually increases in $quality the older it gets
+                $delta = 1;
                 // Once the sell by date has passed, $quality
                 // degrades twice as fast
                 $delta = ($item->sellIn < 0) ? $delta*2 : $delta;
@@ -36,32 +27,24 @@ class GildedRose {
 
             case 'Backstage passes to a TAFKAL80ETC concert':
 
-                // The $quality of an item is never negative
-                if ($item->quality < 0) {
-                    $delta = 0;
+                if ($item->sellIn < 0) {
+                    // $quality drops to 0 after the concert
+                    $delta = $item->quality * (-1);
                 }
-                //The $quality of an item is never more than 50
-                else if ($item->quality < 50) {
+                elseif ($item->sellIn <= 5) {
+                // $quality increases by 3 when there are 5 days or less
+                    $delta = 3;
+                }
+                elseif ($item->sellIn <= 10) {
+                    // $quality increases by 2 when there are 10 days or less
+                    $delta = 2;
+                }
+                else {
+                    // "Backstage passes", like aged brie, increases in $quality 
+                    // as it's $sellIn value approaches;
                     $delta = 1;
-                } else {
-                    if ($item->sellIn < 0) {
-                        // $quality drops to 0 after the concert
-                        $delta = $item->quality;
-                    }
-                    elseif ($item->sellIn <= 5) {
-                    // $quality increases by 3 when there are 5 days or less
-                        $delta = 3;
-                    }
-                    elseif ($item->sellIn <= 10) {
-                        // $quality increases by 2 when there are 10 days or less
-                        $delta = 2;
-                    }
-                    else {
-                        // "Backstage passes", like aged brie, increases in $quality 
-                        // as it's $sellIn value approaches;
-                        $delta = 1;
-                    }
                 }
+                
                 // Once the sell by date has passed, $quality
                 // degrades twice as fast
                 $delta = ($item->sellIn < 0) ? $delta*2 : $delta;
@@ -85,52 +68,22 @@ class GildedRose {
 
     function updateQuality() {
         foreach ($this->items as $item) {
-            $qualityDelta = $this->getQualityDelta($item);
 
-            if ($item->name != 'Aged Brie' and $item->name != 'Backstage passes to a TAFKAL80ETC concert') {
-                if ($item->quality > 0) {
-                    if ($item->name != 'Sulfuras, Hand of Ragnaros') {
-                        $item->quality = $item->quality + $qualityDelta;
-                    }
-                }
-            } else {
-                if ($item->quality < 50) {
-                    $item->quality = $item->quality + $qualityDelta;
-                    if ($item->name == 'Backstage passes to a TAFKAL80ETC concert') {
-                        if ($item->sellIn < 11) {
-                            if ($item->quality < 50) {
-                                $item->quality = $item->quality + $qualityDelta;
-                            }
-                        }
-                        if ($item->sellIn < 6) {
-                            if ($item->quality < 50) {
-                                $item->quality = $item->quality + $qualityDelta;
-                            }
-                        }
-                    }
-                }
-            }
-            
             if ($item->name != 'Sulfuras, Hand of Ragnaros') {
                 $item->sellIn = $item->sellIn - 1;
             }
 
-            if ($item->sellIn < 0) {
-                if ($item->name != 'Aged Brie') {
-                    if ($item->name != 'Backstage passes to a TAFKAL80ETC concert') {
-                        if ($item->quality > 0) {
-                            if ($item->name != 'Sulfuras, Hand of Ragnaros') {
-                                $item->quality = $item->quality + $qualityDelta;
-                            }
-                        }
-                    } else {
-                        $item->quality = $item->quality - $item->quality;
-                    }
-                } else {
-                    if ($item->quality < 50) {
-                        $item->quality = $item->quality + $qualityDelta;
-                    }
-                }
+            $qualityDelta = $this->getQualityDelta($item);
+            $item->quality = $item->quality + $qualityDelta;
+
+            //The $quality of an item is never more than 50
+            if ($item->name != 'Sulfuras, Hand of Ragnaros' && $item->quality >= 50) {
+                $item->quality = 50;
+            }
+
+            // The $quality of an item is never negative
+            if ($item->quality < 0) {
+                $item->quality = 0;
             }
         }
     }
